6c929ceafbe8e1ca6958ebb48e367ccd9332541a
Nasr Galal <nasrbue@gmail.com>
2021-09-18 20:13:39 +0200

    fix: add missing packages for running mongo

--- config/database.js
+++ config/database.js
@@ -15,9 +15,12 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         uri: env('DATABASE_URI'),
+        // host: env('DATABASE_HOST'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME')
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {}
     }
